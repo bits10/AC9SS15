0dc185ec4c23d2064132883fe87859766c1e5c55
crysxd <c.wuerthner@me.com>
2014-04-09 21:48:30 +0200

    Implemented full REST functionality.
    Implemented the REST functionality with dummy files in /rest. The
    rest.js is yet to be optimized but fully functional. The dummy files in
    /rest may be removed as soon the server supports REST.

--- Webseite/rest.js
+++ Webseite/rest.js
@@ -1,12 +1,128 @@
+var urlValues="rest/values";
+var urlEditable="rest/editable";
+var urlModify="rest/modify?";
+var urlInfo="rest/info";
+var urlModifyIP="rest/setip?ip=";
+var onValuesChanged;
+var cachedInfo;
+var cachedValues;
+var cachedEditable;
+var pollingFreq=1000;
+var realPollingFreq=0;
+var lastPollingTime=Date.now();
+var isInitialised;
+var isPaused=0;
+/*
+ * Inits the REST interface. Should only be called once at the startup of the page!
+ */
+function initRest(){
+	console.log(getInfo().ip);
+	setOnValuesChange(function(values, time){
+		document.getElementById('freq').innerHTML="Frequenz: " + time+"ms";
+	});
+	
+	refreshValues();
+}
+
+/*
+ * Internally used function to refresh the values in an given interval.
+ */
+function refreshValues(){
+	cachedValues=JSON.parse(loadURL(urlValues));
+	
+	realPollingFreq=Date.now()-lastPollingTime;
+	lastPollingTime=Date.now();
+	
+	onValuesChanged(cachedValues, realPollingFreq);	
+	
+	if(!isPaused)
+		window.setTimeout("refreshValues()",pollingFreq);
+}
+
 /**
- * Loads the contents of the given URL and returns the laoded content. This funtion is incompatible with IE 6 and older.
+ * Loads the contents of the given URL and returns the laoded content. 
+ * This funtion is incompatible with IE 6 and older.
  * @param {Object} the URL of the requested resource.
  * @return The loaded content.
  */
 function loadURL(url){
-  	xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET",url,false);
-	xmlhttp.send();
+  	x=new XMLHttpRequest();
+	x.open("GET",url,false);
+	x.send();
 	
-	return xmlhttp.responseText;
+	return x.responseText;
 }
+
+/**
+ * Sets the function invoked when a new value set was recieved by the server. 
+ * The function should have two parameters. The first is the value array recieved 
+ * and the second is the current polling rate in milliseconds.
+ * @param {Object} The function to be invoked after a new value set was recieved.
+ */
+function setOnValuesChange(func){
+	onValuesChanged=func;
+}
+
+/**
+ * Returns the value for the pin or analog input with the id.
+ * @param {Object} the value of the pin or analog input.
+ */
+function getValue(id){
+	return cachedValues[id];
+}
+
+/**
+ * Sets the value for the pin with the id. Has no effect if the pin is not editable.
+ * Returns the answer of the server.
+ * @param {Object} the id of the pin to modify
+ * @param {Object} the new value. Should be 1 or 0.
+ */
+function setValue(id,value){
+	return loadURL(urlModify+id+'='+value);
+}
+
+/**
+ * Returns if the pin with the given id is editable.
+ * @param {Object} The id of the pin.
+ */
+function isEditable(id){
+	if(!cachedEditable)
+		cachedEditable=JSON.parse(loadURL(urlEditable));
+		
+	return cachedEditable[id];
+}
+
+/**
+ * Returns the info array, including 'ip', 'mac' and 'version'.
+ * Usage: getInfo().ip
+ */
+function getInfo(){
+	if(!cachedInfo)
+		cachedInfo=JSON.parse(loadURL(urlInfo));
+		
+	return cachedInfo;
+}
+
+/**
+ * Sets the ip of the board and causes the side to reload.
+ * @param {Object} the new ip of the board
+ */
+function setIP(ip){
+	loadUrl(urlModifyIP+ip);
+	window.location = ip+"/index.html";
+}
+
+/**
+ * Toggles the isPaused field and starts the refreshing process again if 
+ * the pause mode was disabled. Returns true if the pause mode was entered and
+ * false if the pause mode was disabled.
+ */
+function togglePause() {
+	isPaused=!isPaused;
+	
+	if(!isPaused)
+		refreshValues();
+		
+	return isPaused;
+}
+
