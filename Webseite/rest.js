c67bfd10f99935ddce0a5401a2833a60969bcf3b
crysxd <c.wuerthner@me.com>
2014-06-09 10:05:44 +0200

    Code cleanup, values are auto-send with every reload

--- Webseite/rest.js
+++ Webseite/rest.js
@@ -1,68 +1,144 @@
-var urlValues="rest/values";
-var urlPinInfo="rest/pininfo";
-var urlModifyValue="rest/setpin?";
-var urlInfo="rest/info";
+//The URL to query the pin values from
+var urlValues = "rest/values";
+
+//The URL to query the pininfos from
+var urlPinInfo = "rest/pininfo";
+
+//The URL to query the server Information from
+var urlInfo = "rest/info";
+
+//The function to trigger after every value reload
 var onValuesChanged;
+
+//The function to trigger when a network error occurs
 var onNetworkError;
+
+//The last loaded server info (from urlInfo)
 var cachedInfo;
+
+//The last loaded values (from urlValues)
 var cachedValues;
+
+//A real copy of cachedValues to check, if last values was changed
+var cachedValuesReference;
+
+//The last loaded pin infos (from urlPinInfo)
 var cachedPinInfo;
-var pollingFreq=1000;
-var realPollingFreq=0;
-var lastPollingTime=Date.now();
+
+//The currently used polling frequency (should-be value)
+var pollingFreq = 1000;
+
+//The currently measured polling frequency
+var realPollingFreq = 0;
+
+//The time of the last reload
+var lastPollingTime = Date.now();
+
+//A flag indicating if the REST interface is already initialised 
 var isInitialised;
-var taskIdCounter=0;
-/*
+
+//A counter to count the refresh tasks
+var taskIdCounter = 0;
+
+/**
  * Inits the REST interface. Should only be called once at the startup of the page!
  */
-function initRest(){
-	if(isInitialised)
+function initRest() {
+    //Check if the REST interface is already inititalised and cancel if true
+	if (isInitialised) {
 		return;
+    }
 	
+    //Init the onValuesChanged variable with an empty function
 	setOnValuesChanged(function(pinInfo, values, time){});
 	
+    //Init the onNetworkError variable with an empty function
 	setOnNetworkError(function(state, response){});
 	
-	cachedInfo=JSON.parse(loadURL(urlInfo));
-	cachedPinInfo=JSON.parse(loadURL(urlPinInfo));
-	cachedValues=JSON.parse(loadURL(urlValues));
+    //Load the server info, this must only happen once
+	cachedInfo = JSON.parse(loadURL(urlInfo));
+    
+    //Load the pin info, this must only happen once
+	cachedPinInfo = JSON.parse(loadURL(urlPinInfo));
+    
+    //Load the values for the first time
+	cachedValues = JSON.parse(loadURL(urlValues));
+    
+    //Load the pollingFrequncy to be used from the db module
+    pollingFreq = getDb('pollingFreq', 1000);
+
+    //Start a refresh task which will update the values after a delay (pollingFrequency)
 	startNewRefershTask();
-    pollingFreq=getDb('pollingFreq', 1000);
-	isInitialised=1;
+    
+    //Set initialised to true to prevent reinitialisation
+	isInitialised = 1;
+    
 }
 
+/**
+ * Cancels the currently running refersh task.
+ */
 function cancelRefreshTask() {
+    //Increment taskIdCounter to signal the refesh task after waking up from setTimeout() that is not anymore used
     ++taskIdCounter;
+    
 }
+
+/**
+ * Cancels the currently running refresh task and starts a new one.
+ * This will cause the values to be refreshed immediately.
+ * @param {Object} The post parameters to send with the first refresh
+ */
 function startNewRefershTask(postParams) {
 	 refreshValues(++taskIdCounter, postParams);
 }
-/*function cancelRefreshTask() {
-	taskIdCounter++;
-}*/
+
 /*
  * Internally used function to refresh the values in an given interval.
  */
 function refreshValues(taskId, postParams){
-	if(taskIdCounter!=taskId) {
-		console.log("Task canceled!");
+    //Check if this task is still in use or if the taskIdCounter was raised
+	if(taskIdCounter != taskId) {
 		return;
+        
 	}
+    
+    //Check if values or dd was changed, if true call sendValues() and return (send values will start a new refresh task)
+    //If postParams are supplied, just ignore changes becuase the post params will already contain the info to send to the server
+    if(!postParams && JSON.stringify(cachedValues) !== JSON.stringify(cachedValuesReference)) {
+        console.log("Values or DD was changed! (Call sendValues)");
+        sendValues();
+        return;
+    
+    }
 	
+    //Call loadURLAsync to reload the values in background
 	loadURLAsync(urlValues, postParams, function(state, response) {
-		cachedValues=JSON.parse(response);
+        //Measure the real polling frequency and save
+		realPollingFreq = Date.now()-lastPollingTime;
+        
+        //update the last polling time
+		lastPollingTime = Date.now();
 	
-		realPollingFreq=Date.now()-lastPollingTime;
-		lastPollingTime=Date.now();
+        //If everything is OK (HTTP state 200)
+		if(state == 200) {
+            //Parse the response to a object and save in cachedValues
+		    cachedValues = JSON.parse(response);
 	
-		if(state==200) {
-			onValuesChanged(cachedPinInfo, cachedValues, realPollingFreq);	
-            window.setTimeout("refreshValues("+taskId+", null)",pollingFreq);
-				
+            //Create a identically copy
+            cachedValuesReference = JSON.parse(response);
+            
+            //Call onValuesChanged() to notify about the new values
+			onValuesChanged(cachedPinInfo, cachedValues, realPollingFreq);
+            
+            //Set new Timeout to reload the values again
+            window.setTimeout("refreshValues(" + taskId + ", null)", pollingFreq);
+
+        //If something went wrong, call onNetworkError()
 		} else {
 			onNetworkError(state, response);
+            
 		}
-		
 	});
 }
 
@@ -73,33 +149,61 @@ function refreshValues(taskId, postParams){
  * @return The loaded content.
  */
 function loadURL(url){
+    //Create a new XMLHttpRequest
   	x=new XMLHttpRequest();
-	x.open("GET",url,false);
+    
+    //Open a connection to the url using HTTP-GET Method.
+    //false means the loading is synchronosly in the foreground (will block the programm!)
+	x.open("GET", url, false);
+    
+    //Send the request
 	x.send();
 	
+    //return the response
 	return x.responseText;
+    
 }
 
 /**
  * Loads the contents of the given URL and returns the laoded content. 
  * This funtion is incompatible with IE 6 and older.
  * @param {Object} the URL of the requested resource.
+ * @param {Object} the post parameters to send along with the request or null
+ * @param {Function} the function to call after the transmission is done. Should have two params: the http status (200, 404...) and the laoded content
  * @return The loaded content.
  */
 function loadURLAsync(url, postParams, func){
-  	x=new XMLHttpRequest();
+    //Create a new XMLHttpRequest
+  	x = new XMLHttpRequest();
+    
+    //Set a onreadystatechanged function to be notified about status updates (connection openened, transmission done...)
   	x.onreadystatechange=function() {
-  		if(x.readyState==4)
+        //If the new state is done call the given function
+  		if(x.readyState == 4)
   			func(x.status,x.responseText);
+        
   	};
 	
+    //If post parameters where given
 	if(postParams) {
-        x.open("POST",url,true);
+        //Open a connection to the url using HTTP-POST Method.
+        //true means the connection is in a background thread
+        x.open("POST", url, true);
+        
+        //Set content type for POST-Parameters
 		x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');	
+        
+        //Send the request
 		x.send(postParams);
+        
 	} else {
+        //Open a connection to the url using HTTP-GET Method.
+        //true means the connection is in a background thread
         x.open("GET",url,true);
+        
+        //Send the request
 		x.send();
+        
 	}
 }
 
@@ -110,7 +214,7 @@ function loadURLAsync(url, postParams, func){
  * @param {Object} The function to be invoked after a new value set was recieved.
  */
 function setOnValuesChanged(func){
-	onValuesChanged=func;
+	onValuesChanged = func;
 }
 
 /**
@@ -120,7 +224,7 @@ function setOnValuesChanged(func){
  * @param {Object} The function to be invoked when a network error occured.
  */
 function setOnNetworkError(func){
-	onNetworkError=func;
+	onNetworkError = func;
 }
 
 /**
@@ -131,6 +235,12 @@ function getValue(id){
 	return cachedValues[getIndex(id)].v;
 }
 
+/**
+ * Sets the value of the pin with the given id.
+ * @param {Object} the id of the pin which value should be set ('C0' etc.)
+ * @param {Object} the new value 
+ * @param {Object} a flag indicating if the values should be applied immediately or with the next reload
+ */
 function setValue(id, value, apply) {
     cachedValues[getIndex(id)].v = value;
 
@@ -150,17 +260,17 @@ function sendValues(){
 	var newValues = new Object();
 	for(var k in cachedValues) {
 		var id = getId(k);
-		var num=id.charAt(1);
-		var port=id.charAt(0);
+		var num = id.charAt(1);
+		var port = id.charAt(0);
 		
 		if(!newValues[port]) {
-			newValues[port]=new Array(8);
+			newValues[port] = new Array(8);
 			for (var i=0; i<newValues[port].length; i++) 
 				newValues[port][i] = 0; 
 		}
 			
 		if(getDD(id) == 'o') {
-			newValues[port][num]=cachedValues[getIndex(id)].v;
+			newValues[port][num] = cachedValues[getIndex(id)].v;
 		} 
 	}
 	
@@ -168,10 +278,10 @@ function sendValues(){
 	for(var k in newValues) {
 		var hex = 0;
 		for(var i=0; i<newValues[k].length; i++) {
-            if(newValues[k][i]==true)
-                newValues[k][i]=1;
+            if(newValues[k][i] == true)
+                newValues[k][i] = 1;
             else
-                newValues[k][i]=0;
+                newValues[k][i] = 0;
 			hex += newValues[k][i] << i;
 		}
 		
@@ -188,6 +298,55 @@ function sendValues(){
 	post += "SUB=Senden";
 	startNewRefershTask(post);
 	
+}
+
+/**
+ * Sets the data direction of the given pin.
+ * @param {Object} The id of the pin.
+ * @param {Object} The new data direction.
+ */
+function setDD(id, dd) {
+    cancelRefreshTask();
+	var newValues = new Object();
+	for(var k in cachedValues) {
+		var kid = getId(k);
+		var num=kid.charAt(1);
+		var port=kid.charAt(0);
+		
+		if(!newValues[port]) {
+			newValues[port] = new Array(8);
+			for(var i=0; i<newValues[port].length; i++) 
+				newValues[port][i] = 0; 
+		}
+			
+        newValues[port][num] = (cachedValues[k].dd == 'o') + 0;
+        
+        if(id == kid) {
+            newValues[port][num] = (dd == 'o') + 0;
+        }
+	}
+	
+    console.log(newValues);
+	var post = "";
+	for(var k in newValues) {
+		var hex = 0;
+		for(var i=0; i<newValues[k].length; i++) {
+			hex += newValues[k][i] << i;
+		}
+		
+		hex = hex.toString(16);
+		
+		while(hex.length<2)
+            hex = "0" + hex;
+			
+		post += "SET=OUT" + k + hex + "&";
+
+	}
+	
+    post = post.toUpperCase();
+	post += "SUB=Senden";
+    startNewRefershTask(post);
+
 }
 
 /**
@@ -230,8 +389,8 @@ function getDDOptions(id){
  */
 function getDDDescription(dd) {
 	switch(dd){
-		case 'i':return"Eingang";
-		case 'o':return"Ausgang";
+		case 'i': return"Eingang";
+		case 'o': return"Ausgang";
 	}
 }
 
@@ -243,57 +402,6 @@ function getDDDescription(dd) {
  */
 function getDD(id){
 	return  cachedValues[getIndex(id)].dd;
-}
-
-/**
- * Sets the data direction of the given pin.
- * @param {Object} The id of the pin.
- * @param {Object} The new data direction.
- */
-function setDD(id, dd) {
-    cancelRefreshTask();
-	var newValues = new Object();
-	for(var k in cachedValues) {
-		var kid = getId(k);
-        console.log(i);
-		var num=kid.charAt(1);
-		var port=kid.charAt(0);
-		
-		if(!newValues[port]) {
-            console.log("new:"+port);
-			newValues[port]=new Array(8);
-			for(var i=0; i<newValues[port].length; i++) 
-				newValues[port][i] = 0; 
-		}
-			
-        newValues[port][num]=(cachedValues[k].dd=='o')+0;
-        
-        if(id == kid) {
-            newValues[port][num]=(dd=='o')+0;
-        }
-	}
-	
-    console.log(newValues);
-	var post = "";
-	for(var k in newValues) {
-		var hex = 0;
-		for(var i=0; i<newValues[k].length; i++) {
-			hex += newValues[k][i] << i;
-		}
-		
-		hex = hex.toString(16);
-		
-		while(hex.length<2)
-            hex = "0" + hex;
-			
-		post += "SET=OUT" + k + hex + "&";
-
-	}
-	
-    post = post.toUpperCase();
-	post += "SUB=Senden";
-    startNewRefershTask(post);
-
 }
 
 /**
@@ -326,12 +434,18 @@ function getId(index){
  * @return true if the operation was successfull, false otherwise
  */
 function setPollingFreq(freq){
+    //If freq is a number
 	if(!isNaN(parseFloat(freq)) && isFinite(freq)) {
-		pollingFreq=freq;
+        //Set polling freq
+		pollingFreq = freq;
+        //Save in db
         putDb('pollingFreq', pollingFreq);
+        
+        //return true (success)
 		return true;
 	}
 	
+    //return false (no success)
 	return false;
 }
 
@@ -366,7 +480,7 @@ function getId(index){
  */
 function getIndex(id){
 	for(var i=0;i<cachedPinInfo.length;i++)
-		if(cachedPinInfo[i].id==id)
+		if(cachedPinInfo[i].id == id)
 			return i;
 			
 	return -1;
