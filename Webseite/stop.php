dcab2dde804f1bfb6676189aedf2f36acbdc91e9
Timo Bayer <timo.bayer@hs-furtwangen.de>
2015-05-06 14:42:16 +0200

    cleanup

--- Webseite/stop.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$id =  $_GET['ad'];
-	$verbindung = mysql_connect("localhost", "root", "ProjektSS15");
-	mysql_select_db("AVR") or die("Die Datenbank existiert nicht.");
-	$sql = "update AVR.Anwendungen set status = '0' where id = '$id'";
-	$ergebnis = mysql_query($sql);
-?>	
